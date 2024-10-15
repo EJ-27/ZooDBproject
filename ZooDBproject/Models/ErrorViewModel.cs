644da637f01c418b0f66bfc1a8101998247ff996
Jarad <Jarad@HASSAN>
2024-10-15 14:28:41 -0500

    Add project files.

---
+++ ZooDBproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZooDBproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
