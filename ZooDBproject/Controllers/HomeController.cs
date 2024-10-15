7638380a854d06b7d3460bbd26b7fccabd5fc3ec
EJ <syjarada@CougarNet.UH.EDU>
2024-10-15 15:10:06 -0500

    UPDATE:
    1. created new temp page "TestTab" in Views\Home
    
    2. created corresponding header and footer in Views\Shared\_Layout.cshtml
    
    3. Modified Controllers\HomeConstroller.cs to include a view page response when TestTab is clicked
    
    -Evan

--- ZooDBproject/Controllers/HomeController.cs
+++ ZooDBproject/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ZooDBproject.Controllers
             return View();
         }
 
+        public IActionResult TestTab()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
